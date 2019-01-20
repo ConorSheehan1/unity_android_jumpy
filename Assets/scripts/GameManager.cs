6efdca57bb605cf1dd29818699651df0ee691590
conor <conor.sheehan@live.ie>
2019-01-20 17:53:26 +0000

    add night sky background, restructure project

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public void EndGame()
+	{
+		Debug.Log ("game over");
+		SceneManager.LoadScene ("GameOver");
+	}
+}
---
+++ Assets/scripts/UserControls.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UserControls : MonoBehaviour {
+	public Rigidbody2D rb;
+	// ground
+	//public Transform groundCheck;
+	//public float groundCheckRandius;
+	//public LayerMask isGround;
+	//private bool onGround;
+
+	//space
+
+	public Transform spaceCheck;
+	public float spaceCheckRadius;
+	public LayerMask isSpace;
+	private bool inSpace;
+	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2(3, rb.velocity.y);
+
+		//onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRandius, isGround);
+		inSpace = Physics2D.OverlapCircle(spaceCheck.position, spaceCheckRadius, isSpace);
+
+		if (Input.GetMouseButtonDown(0)) {
+			rb.velocity = new Vector2(rb.velocity.x, 3);
+		}
+
+		if (!inSpace) {
+			FindObjectOfType<GameManager> ().EndGame();
+			// game over
+		}
+
+	}
+}
